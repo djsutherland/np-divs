c6dd01831c6d6e010c1eb3a105d0678e2e1ece59
Dougal Sutherland <dougal@gmail.com>
2012-01-18 15:09:33 -0500

    flann::L2 is the *squared* euclidean distance, silly

--- dkn.hpp
+++ dkn.hpp
@@ -2,6 +2,7 @@
 #define DKN_HPP_
 #include "basics.hpp"
 
+#include <cmath>
 #include <vector>
 #include <flann/flann.hpp>
 
@@ -10,10 +11,15 @@ std::vector<typename Distance::ResultType> DKN(
         flann::Index<Distance> &index,
         const flann::Matrix<typename Distance::ElementType> &query,
         int k = 3,
-        const flann::SearchParams &search_params = flann::SearchParams())
+        const flann::SearchParams &search_params = flann::SearchParams(),
+        bool take_sqrt = true)
 {   /* Get the distances to the k-th nearest neighbor of each element in query
      * using the passed index and search params.
-     */
+     *
+     * Because flann::L2 is actually the squared Euclidean distance, this
+     * function by default square-roots the results. Pass take_sqrt=false to
+     * avoid this.
+     * */
     using std::vector;
     using flann::Matrix;
 
@@ -32,9 +38,12 @@ std::vector<typename Distance::ResultType> DKN(
     // get out just the results we want
     vector<DistanceType> dkn;
     dkn.reserve(query.rows);
-    for (size_t i = 0; i < query.rows; i++) {
-        dkn.push_back(dists[i][k-1]);
-    }
+    if (take_sqrt)
+        for (size_t i = 0; i < query.rows; i++)
+            dkn.push_back(std::sqrt(dists[i][k-1]));
+    else
+        for (size_t i = 0; i < query.rows; i++)
+            dkn.push_back(dists[i][k-1]);
 
     delete[] indices.ptr();
     delete[] dists.ptr();
