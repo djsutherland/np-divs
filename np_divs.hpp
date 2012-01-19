794b59ef936c9a10c83e58b3b2ee4a186d59b7fa
Dougal Sutherland <dougal@gmail.com>
2012-01-18 19:51:43 -0500

    avoid repeating work when comparing bags to themselves

--- np_divs.hpp
+++ np_divs.hpp
@@ -23,10 +23,7 @@ void np_divs(
         flann::Matrix<Scalar>* results,
         int k = 3)
 {
-    boost::ptr_vector<DivFunc> div_funcs;
-    div_funcs.push_back(new DivL2());
-
-    return np_divs(bags, num_bags, div_funcs, results, k);
+    return np_divs(bags, num_bags, NULL, num_bags, results, k);
 }
 
 template <typename Scalar>
@@ -36,8 +33,8 @@ void np_divs(
         flann::Matrix<Scalar>* results,
         int k = 3)
 {
-    // TODO - do this in a way that doesn't do unnecessary work
-    return np_divs(bags, num_bags, bags, num_bags, div_funcs, results, k);
+    return np_divs(bags, num_bags, (flann::Matrix<Scalar>*) NULL, num_bags,
+            div_funcs, results, k);
 }
 
 template <typename Scalar>
@@ -58,8 +55,8 @@ void np_divs(
 
 template <typename Scalar>
 void np_divs(
-    const flann::Matrix<Scalar>* &x_bags, size_t num_x,
-    const flann::Matrix<Scalar>* &y_bags, size_t num_y,
+    const flann::Matrix<Scalar>* x_bags, size_t num_x,
+    const flann::Matrix<Scalar>* y_bags, size_t num_y,
     const boost::ptr_vector<DivFunc> &div_funcs,
     flann::Matrix<float>* results,
     int k = 3,
@@ -83,13 +80,15 @@ void np_divs(
     using std::vector;
 
     typedef flann::L2<Scalar> Distance;
-    typedef typename Distance::ResultType DistanceType;
 
     typedef flann::Matrix<Scalar> Matrix;
     typedef flann::Index<Distance> Index;
+    typedef vector<typename Distance::ResultType> DistVec;
 
     int num_dfs = div_funcs.size();
     int dim = x_bags[0].cols;
+
+    bool bags_are_same = y_bags == NULL || x_bags == y_bags;
 
     // check that result matrices are allocated properly
     if (verify_results_alloced) {
@@ -106,50 +105,81 @@ void np_divs(
 
     // build flann::Index objects for the bags
     Index** x_indices = (Index**) malloc(sizeof(Index*) * num_x);
+    Index** y_indices;
+
     for (size_t i = 0; i < num_x; i++) {
         Index* idx = new Index(x_bags[i], index_params);
         idx->buildIndex();
         x_indices[i] = idx;
     }
 
-    Index** y_indices = (Index**) malloc(sizeof(Index*) * num_y);
-    for (size_t i = 0; i < num_y; i++) {
-        Index* idx = new Index(y_bags[i], index_params);
-        idx->buildIndex();
-        y_indices[i] = idx;
+    if (!bags_are_same) {
+        y_indices = (Index**) malloc(sizeof(Index*) * num_y);
+        for (size_t i = 0; i < num_y; i++) {
+            Index* idx = new Index(y_bags[i], index_params);
+            idx->buildIndex();
+            y_indices[i] = idx;
+        }
     }
 
     // make rhos for the bags
     // TODO - check that we actually need the y_rhos
-    vector<vector<DistanceType> > x_rhos, y_rhos;
-
+    vector<DistVec> x_rhos, y_rhos;
     x_rhos.reserve(num_x);
     for (size_t i = 0; i < num_x; i++)
         x_rhos.push_back(DKN(*x_indices[i], x_bags[i], k+1, search_params));
 
-    y_rhos.reserve(num_y);
-    for (size_t j = 0; j < num_y; j++)
-        y_rhos.push_back(DKN(*y_indices[j], y_bags[j], k+1, search_params));
+    if (!bags_are_same) {
+        y_rhos.reserve(num_y);
+        for (size_t j = 0; j < num_y; j++)
+            y_rhos.push_back(DKN(*y_indices[j], y_bags[j], k+1, search_params));
+    }
 
-    // compute the divergences!
-    Matrix x_bag, y_bag;
-    vector<DistanceType> rho_x, nu_x, rho_y, nu_y;
+    // compute the divergences!  // TODO: threading
+    DistVec nu_x, nu_y;
 
-    // TODO: threading
-    for (size_t i = 0; i < num_x; i++) {
-        rho_x = x_rhos[i]; // TODO - is this copying????
+    // TODO - check that we actually need nu_y
 
-        for (size_t j = 0; j < num_y; j++) {
-            rho_y = y_rhos[j];
+    if (!bags_are_same) {
+        for (size_t i = 0; i < num_x; i++) {
+            DistVec &rho_x = x_rhos[i];
+            for (size_t j = 0; j < num_y; j++) {
+                DistVec &rho_y = y_rhos[j];
 
-            nu_x = DKN(*y_indices[j], x_bags[i], k, search_params);
-            nu_y = DKN(*x_indices[i], y_bags[j], k, search_params);
-            // TODO - check that we actually need nu_y
-            // TODO - if x_bags = y_bags, do both dirs in here at the same time
+                DistVec nu_x = DKN(*y_indices[j], x_bags[i], k, search_params);
+                nu_y = DKN(*x_indices[i], y_bags[j], k, search_params);
 
+                for (size_t df = 0; df < num_dfs; df++) {
+                    results[df][i][j] = div_funcs[df](
+                            rho_x, nu_x, rho_y, nu_y, dim, k);
+                }
+            }
+        }
+    } else {
+        // we can save some nearest-neighbor calculation by calculating both
+        // directions at the same time
+        for (size_t i = 0; i < num_x; i++) {
+            DistVec &rho_x = x_rhos[i];
+
+            // compare with self
             for (size_t df = 0; df < num_dfs; df++) {
-                float res = div_funcs[df](rho_x, nu_x, rho_y, nu_y, dim, k);
-                results[df][i][j] = res;
+                results[df][i][i] = div_funcs[df](
+                        rho_x, rho_x, rho_x, rho_x, dim, k);
+            }
+
+            // compare with others
+            for (size_t j = 0; j <= i; j++) {
+                DistVec &rho_y = x_rhos[j];
+
+                nu_x = DKN(*x_indices[j], x_bags[i], k, search_params);
+                nu_y = DKN(*x_indices[i], x_bags[j], k, search_params);
+
+                for (size_t df = 0; df < num_dfs; df++) {
+                    results[df][i][j] = div_funcs[df](
+                            rho_x, nu_x, rho_y, nu_y, dim, k);
+                    results[df][j][i] = div_funcs[df](
+                            rho_y, nu_y, rho_x, nu_x, dim, k);
+                }
             }
         }
     }
@@ -161,10 +191,13 @@ void np_divs(
 
     for (size_t i = 0; i < num_x; i++)
         delete x_indices[i];
-    for (size_t i = 0; i < num_y; i++)
-        delete y_indices[i];
     free(x_indices);
-    free(y_indices);
+
+    if (!bags_are_same) {
+        for (size_t i = 0; i < num_y; i++)
+            delete y_indices[i];
+        free(y_indices);
+    }
 }
 
 }
